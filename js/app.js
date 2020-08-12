3694f244f64817d60bdf360edd6569b0d52a7448
Eduardo Campos <eduardo_c.s@outlook.com>
2020-08-12 02:20:23 -0500

    Tipos de datos y anotaciones

--- js/app.js
+++ js/app.js
@@ -1,20 +1,108 @@
 "use strict"
 
 /*
-las variables son de tipo de destructivas, si se le ingresa primero un valor, y despues se le ingresa otro valor, el primer valor desaparecerá y no se podrá recuperar de ninguna manera
+Tipo de datos
 
-Para defninir el alcance de una variable existen dos formas de definirlo, una con la palabra reservada var, que esta se convierte en una variable global o sea que no importa si existe dentro de una funcion o una estructura de datos, y si se quiere utilizar una variable de bloque o variable local e deberá utilizar la palabra reservada let ya sea q se quiera utilizar solamente en una funcio o estructura de datos
+-Numericos
+En javascript se pueden utilizar datos de tipo numericos como enteros, tambien flotantes, o numeros negativos sin necesidad de utilizar las comillas
+Hay que tener en cuenta que no si se desean realizar una operacion entre dos cantidades pero una está dentro de comillas el resultado final de esa operación será la concatenación de esos dos valores. Ejemplo:
+
+var cantidad="100";
+
+(Resultado de la suma desde la consola del navegador)
+cantidad+20
+10020
+
+Para poder realizar una operación correctamente se puede parsear esa variable en lo el tipo de dato deseado en este caso numerico, para esto se utiliza la funcion Number(valor). Ejemplo
+
+var cantidad="100";
+var nuevaCantidad=Number(cantidad);
+
+Tambien existen otras funciones como:
+parseInt()
+parseFloat()
+
+Estas funciones convierten las cadenas ya sea en enteros o flotantes respectivamente
+
+-Cadenas
+
+Para utilizar una cadena de texto se pueden utilizar comillas sencillas o comillas dobles siempre teniendo en cuenta que si se inicia con comillas sencillas se debe de terminar con comillas sencillas, al igual que si se inicia con comillas dobles se debe de terminar con comillas dobles.
+Si se intenta utilizar un número como cadena, se puede utilizar la función String() para convertilo a cadena.
+
+-Boleanos
+
+Para declarar un tipo de dato boleano bastará con utilizar un true o un false
+Tambien se puede utilizar la funcion Boolean para evaluar cualquier cosa. Ejemplo
+
+var esMayor=Boolean(10>9);
+
+-Fechas
+
+Para obtener la fecha bastará con utilizar la funcion new Date. Ejemplo
+
+var fecha= new Date();
+
+Si desde consola se obtiene el valor de variable fecha, esta devolvera el día, el mes, el año, la hora, el minuto y el segundo en el se invocó la funciónpor primera vez, este valor no cambiará aunque se invoque en otro momento la variable, si se quiere generar una nueva fecha se tendrá que utilizar de nuevo la función new Date();-
+Al utilizar el tipo de dato Date, este contiene metodos con el cual podemos saber que día de la semana es, o que mes es, que año es, entre muchas otros valores, esto se puede obtener de dos maneras, directamente poniendo un punto despues del parentesis y poner la función que se va a utilizar o cuando se llame a la variable enseguida poner un punto y poner la funcion a utilizar. Ejemplo
+
+var dia= new Date().Date();
+var mes= fecha.getMonth();
+
+-Simbolos
+
+Estos solo bastaran con utilizar Symbol(); este tipo de datos se puede utilizar cuando se manejan varios ambientes y se quiere saber en que ambiente está.
+
+var simbolo=Symbol();
+var ambiente=Symbol("deb");
+
+Cuando se utiliza los simbolos, aunque se tengan declarados dos de ellos iguales en diferentes variables, su valor no es el mismo, Ejemplo
+
+var simbolo1=Symbol();
+var simbolo2=Symbol();
+
+(Resultado de la comparación desde la consola del navegador)
+simbolo1==simbolo2
+false
+
+-Json
+
+se declaran los objetos para un Json. Ejemplo
+
+var person={Name:"Eduardo", Edad:27};
+
+var persons=[
+    {Name:"Eduardo", Edad:27},
+    {Name:"Lalo", Edad:27},
+    {Name:"Edward", Edad:27}
+]
+ para convertir o serializar el o los objetos a formato JSON bastará con utilizar JSON.stringify(objeto), y para convertir o deserializar un Json a un objeto bastará con utilizar la funcion JSON.parse(json). Ejemplo
+
+ var personJSON=JSON.stringify(person);
+
+ var newPerson=JSON.parse(personJSON);
 
 */
-var nombre="Eduardo";
 
-console.log(nombre);
+var edad=27;
+var calificacion=9.8;
+var menosUno=-1;
+var cantidad="100";
+var nuevaCantidad=Number(cantidad);
 
-function saludo(){
-    let nombre="Campos";
-    console.log(nombre);
+var textoEdad=String(edad);
 
-    let edad=27;
-    console.log(edad);
-}
-console.log(edad);
+var activo=true;
+var esMayor=Boolean(10>9);
+
+var fecha= new Date();
+var dia= new Date().getDate();
+var mes= fecha.getMonth();
+
+var person={Name:"Eduardo", Edad:27};
+var persons=[
+    {Name:"Eduardo", Edad:27},
+    {Name:"Lalo", Edad:27},
+    {Name:"Edward", Edad:27}
+];
+var personJSON=JSON.stringify(person);
+var newPerson=JSON.parse(personJSON);
