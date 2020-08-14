0cebf964c6f628a71bcf9d1c755996342083f4f1
Eduardo Campos <eduardo_c.s@outlook.com>
2020-08-14 17:39:24 -0500

    Ciclos

--- js/app.js
+++ js/app.js
@@ -1,13 +1,41 @@
 "use strict"
 
 /*
-Operador ternario o condicional
+Los podemos encontrar con el nombre de Ciclos, Loops, o Iteraciones.
 
-var resultado = Condición ? si el resultado es verdadero : si el resultado es falso;
+Existen 2 tipos de ciclos, definidos y los indefinidos
+
+Definidos: for
+Indefinidos: do, do-while
+
+for(incialización del contador; condición, incremento o decremento){
+    acciones
+}
+
+Un ciclo while evalua y dependiendo de esa evaluación puede que entre o no entre a ejecutarse las acciones dentro del ciclo
+while(condicion){
+    acciones
+}
+
+En cambio un ciclo do-while por lo menos se va a ejecutar una sola vez y despues evaluará si sigue con el ciclo o lo termina
+do{
+    acciones
+}while(condición)
 
 */
-var datoA=10;
-var datoB=8;
 
-var resultado = datoA>datoB ? "el dato A es mayor que el dato B" : "el dato B es mayor que el dato A";
-var resultado2 = datoA>datoB ? true : false;
+var productos=5;
+
+for (let contador = 0; contador < productos; contador++) {
+    console.log("Producto #"+productos);
+    //debugger;   cuando se utiliza el "debugger" este realiza un debug en el navegador, pero solamente lo hace cuando la instrucción llega a el
+}
+
+while(productos>0){
+    console.log("Producto vendido");
+}
+
+do{
+    console.log("producto vendido");
+    productos--;
+}while(productos>=1)
